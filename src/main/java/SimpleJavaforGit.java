b663f62420b89f5aa9b745e3237383e9a634cff0
gdpreethi <gd.preethi@gmail.com>
2025-04-03 16:19:49 +0530

    Update SimpleJavaforGit.java
    added sysout

--- src/main/java/SimpleJavaforGit.java
+++ src/main/java/SimpleJavaforGit.java
@@ -4,6 +4,7 @@ public class SimpleJavaforGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("inside simple Java for Git sysout1");
+		System.out.println("Added this line from github");
 	}
 
 }
