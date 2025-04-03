c188fa6a2e9d6b126aaaa246975c89cda948109d
sathi <sathi@kavinesh>
2025-04-03 16:16:29 +0530

    new java file added

---
+++ src/main/java/SimpleJavaforGit.java
@@ -0,0 +1,9 @@
+
+public class SimpleJavaforGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("inside simple Java for Git sysout1");
+	}
+
+}
